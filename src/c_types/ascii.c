251e4b4158f1e4f18058455315f13510751a1cde
Rob Marano <robmarano@gmail.com>
2020-09-15 23:29:56 -0400

    Added wrapping to ascii.c

--- src/c_types/ascii.c
+++ src/c_types/ascii.c
@@ -6,6 +6,8 @@ int main() {
 	c = 'D'; /* variable definition */
 
 	printf("c = %c (decimal)\n", c);
+	/* example of wrapping using the data type limits; think of modulo */
+	printf("c = %c (decimal) after adding %d to %d\n", c, 256, c);
 	printf("c = %x (hex)\n", c);
 	printf("c = %o (octal)\n", c);
 
