1ec76fd768dfd3963333938f6e77780d21364ca2
Rob Marano <robmarano@gmail.com>
2020-11-03 19:47:43 -0500

    Updated struct main.c to showcase using pointers with struct

--- src/struct/main.c
+++ src/struct/main.c
@@ -9,10 +9,23 @@ int main(int argc, char *argv[])
     struct point pt2 = { 2, 1};
     struct rectangle rect = { pt1, pt2 };
 
+    struct rectangle *pRect;
+
+
     double distance = getDistanceFromRect(rect);
 
     printf("Rectangle defined as pt1 = { %d, %d } and pt2 = { %d, %d }\n",
             rect.pt1.x, rect.pt1.y, rect.pt2.x, rect.pt2.y);
+    printf("Distance between points = %1.10e\n", distance);
+
+    pRect = &rect;
+    struct point pt3 = {4,4};
+    pRect->pt2 = pt3;
+
+    distance = getDistanceFromRect(*pRect);
+
+    printf("Rectangle defined as pt1 = { %d, %d } and pt2 = { %d, %d }\n",
+            pRect->pt1.x, pRect->pt1.y, pRect->pt2.x, pRect->pt2.y);
     printf("Distance between points = %1.10e\n", distance);
 
     return(0);
