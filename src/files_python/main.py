340a7d91812c2e02f3564e14a615d95175dff6b8
Rob Marano <robmarano@gmail.com>
2020-12-01 19:31:17 -0500

    First try at working with files in python

--- src/files_python/main.py
+++ src/files_python/main.py
@@ -1,13 +1,33 @@
+# main.py
+
+# Globals
+FILENAME='workfile'
+SIZE = 8
+
+# Functions
 
 def main():
+    with open(FILENAME) as f:
+        all_lines = f.readlines()
+    for line in all_lines:
+        print(line, end='')
+
+def main2():
+    with open(FILENAME) as f:
+        for line in f:
+            print(line, end='')
+
+def main1():
     #This is a function
-    f = open('workfile', 'w')
+#    f = open('workfile', 'r')
+#    f.close()
     with open('workfile') as f:
-        read_data = f.read()
+        read_data = f.read(SIZE)
         print(read_data)
     # We can check that the file has been automatically closed.
-    f.closed
+    print('File is closed = {}'.format(f.closed))
 
+# Program Main
 if __name__ == "__main__":
     # This is your program's main function
     main()
